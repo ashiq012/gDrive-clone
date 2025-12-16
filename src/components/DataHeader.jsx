1a7695d2a37f85a91a3a2039c077a2ced32b53c7
ashiq012 <ashiqali9136@gmail.com>
2025-12-16 11:36:58 +0530

    working on ui

--- src/components/DataHeader.jsx
+++ src/components/DataHeader.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 function DataHeader() {
   return (
-    <div className='h-5 w-full flex justify-between px-5 py-10 border-gray-500 border-b'>
+    <div className='h-5 w-full'>
+      <div className=' flex justify-between px-5 py-3'>
         <div className='text-2xl'>      
                 My Drive
             <ArrowDropDownIcon/>
@@ -13,6 +15,34 @@ function DataHeader() {
             <FormatListBulletedIcon style={{fontSize:"30px"}}/>
             <InfoOutlineIcon style={{padding:"3px",fontSize:"35px"}}/>
         </div>
+        
+    </div>
+    <div className='grid grid-cols-4 p-4 font-semibold pl-19'>
+              <div className='flex flex-col items-center'>
+                <InsertDriveFileIcon style={{fontSize:"70px",color:"gray"}}/>
+                <p>My File</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                <InsertDriveFileIcon style={{fontSize:"70px",color:"gray"}}/>
+                <p>My File</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                <InsertDriveFileIcon style={{fontSize:"70px",color:"gray"}}/>
+                <p>My File</p>
+              </div>
+              <div className='flex flex-col items-center'>
+                <InsertDriveFileIcon style={{fontSize:"70px",color:"gray"}}/>
+                <p>My File</p>
+              </div>
+    </div>
+    <div>
+          <div className='grid grid-cols-4 p-2 font-semibold border-b border-gray-400'>
+              <div className='flex flex-col items-center'>Name</div>
+              <div className='flex flex-col items-center'>Owner</div>
+              <div className='flex flex-col items-center'>Date</div>
+              <div className='flex flex-col items-center'>File Size</div>
+          </div>
+    </div>
     </div>
   )
 }
